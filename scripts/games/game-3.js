a86d4fbd3f18528bcac8deac5550c2e4a8566672
or-cl <148771993+or-cl@users.noreply.github.com>
2024-05-31 05:53:10 +0800

    Update game-3.js

--- scripts/games/game-3.js
+++ scripts/games/game-3.js
@@ -1,7 +1,31 @@
 const cards = document.querySelectorAll('.card-wrapper');
+const cardFinished = document.getElementById('game-three-finish');
+
+cardFinished.addEventListener('click', gameThreeEnd);
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
       card.classList.toggle('flip-card');
+      ACTIVE_GAME_SCORE++;
+      areAllCardsFlipped();
     });
 });
+
+function areAllCardsFlipped() {
+    const cards = document.querySelectorAll('.card-wrapper');
+    
+    for (let card of cards) {
+        if (!card.classList.contains('flip-card')) {
+            return false;
+        }
+    }
+    
+    cardFinished.style.display = 'flex';
+    return true;
+}
+
+function gameThreeEnd() {
+    if (areAllCardsFlipped()) {
+        gameIsOver(true);
+    }
+}
